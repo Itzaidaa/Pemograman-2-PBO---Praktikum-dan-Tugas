0e60edafd2db049ad4f2325b63ae91c691866842
Fatimah Azzahra Nur Faidah <itzaidaa@gmail.com>
2025-11-23 11:48:22 +0700

    Add files via upload

---
+++ Output_P4_2_714240046/Output_P4_2_714240046/ChronoSouvenir_714240046.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_P4_2_714240046
+{
+    public class ChronoSouvenir_NPM : Service_NPM
+    {
+        public string EraTag { get; set; }      
+        public bool IsTimeSafe { get; set; }    
+
+        public ChronoSouvenir_NPM(string name, decimal price, string eraTag, bool isTimeSafe)
+            : base(name, price)
+        {
+            EraTag = eraTag;
+            IsTimeSafe = isTimeSafe;
+        }
+
+        public override void Describe()
+        {
+            string safety = IsTimeSafe ? "AMAN timeline ✅" : "BERISIKO paradox ⚠️";
+            Console.WriteLine($"🪙 Suvenir era: {EraTag}. Status: {safety}.");
+        }
+    }
+}
---
+++ Output_P4_2_714240046/Output_P4_2_714240046/Service_714240046.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_P4_2_714240046
+{
+    public abstract class Service_NPM
+    {
+        
+        private string name;
+        private decimal price;
+
+        
+        protected Service_NPM(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value ?? "Unnamed"; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value < 0 ? 0 : value; }
+        }
+
+        
+        public void ShowInfo()
+        {
+            Console.WriteLine($"[Layanan] {Name} | Harga: {Price:C}");
+        }
+
+        
+        public abstract void Describe();
+    }
+}

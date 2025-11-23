857d958529153b5576e3a319343c7cb5efbbaf0a
Fatimah Azzahra Nur Faidah <itzaidaa@gmail.com>
2025-11-23 11:38:59 +0700

    Add files via upload

---
+++ P6_4_714240046/P6_4_714240046/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace P6_4_714240046
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumeric = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtChar = new System.Windows.Forms.TextBox();
+            this.txtRequired = new System.Windows.Forms.TextBox();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtUpper = new System.Windows.Forms.TextBox();
+            this.txtLower = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumeric
+            // 
+            this.txtNumeric.Location = new System.Drawing.Point(397, 52);
+            this.txtNumeric.Name = "txtNumeric";
+            this.txtNumeric.Size = new System.Drawing.Size(305, 22);
+            this.txtNumeric.TabIndex = 0;
+            this.txtNumeric.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumeric_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numeric Textbox";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightCyan;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(158, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Char Textbox";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(158, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Comparison A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightCyan;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(158, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Regex Validator";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(158, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Required Validator";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LightCyan;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(158, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 19);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Comparison B";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(158, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 19);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Length Validator";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.LightCyan;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(158, 312);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 19);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Upper Case Textbox";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(158, 350);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(149, 19);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Lower Case Textbox";
+            // 
+            // txtChar
+            // 
+            this.txtChar.Location = new System.Drawing.Point(397, 90);
+            this.txtChar.Name = "txtChar";
+            this.txtChar.Size = new System.Drawing.Size(305, 22);
+            this.txtChar.TabIndex = 10;
+            this.txtChar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChar_KeyPress);
+            // 
+            // txtRequired
+            // 
+            this.txtRequired.Location = new System.Drawing.Point(397, 131);
+            this.txtRequired.Name = "txtRequired";
+            this.txtRequired.Size = new System.Drawing.Size(305, 22);
+            this.txtRequired.TabIndex = 11;
+            this.txtRequired.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRequired_KeyPress);
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(397, 168);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(305, 22);
+            this.txtRegex.TabIndex = 12;
+            this.txtRegex.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRegex_KeyPress);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(397, 205);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(305, 22);
+            this.txtA.TabIndex = 13;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(397, 240);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(305, 22);
+            this.txtB.TabIndex = 14;
+            this.txtB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtB_KeyPress);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(397, 276);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(305, 22);
+            this.txtLength.TabIndex = 15;
+            this.txtLength.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLength_KeyPress);
+            // 
+            // txtUpper
+            // 
+            this.txtUpper.Location = new System.Drawing.Point(397, 312);
+            this.txtUpper.Name = "txtUpper";
+            this.txtUpper.Size = new System.Drawing.Size(305, 22);
+            this.txtUpper.TabIndex = 16;
+            // 
+            // txtLower
+            // 
+            this.txtLower.Location = new System.Drawing.Point(397, 350);
+            this.txtLower.Name = "txtLower";
+            this.txtLower.Size = new System.Drawing.Size(305, 22);
+            this.txtLower.TabIndex = 17;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Pink;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSubmit.Location = new System.Drawing.Point(312, 400);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(220, 38);
+            this.btnSubmit.TabIndex = 18;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(813, 464);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtLower);
+            this.Controls.Add(this.txtUpper);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.txtRequired);
+            this.Controls.Add(this.txtChar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumeric);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeric;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtChar;
+        private System.Windows.Forms.TextBox txtRequired;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtUpper;
+        private System.Windows.Forms.TextBox txtLower;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ P6_4_714240046/P6_4_714240046/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P6_4_714240046
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumeric_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+                e.Handled = true;
+        }
+
+        private void txtChar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && e.KeyChar != ' ' && e.KeyChar != (char)Keys.Back)
+                e.Handled = true;
+        }
+
+        private void txtRequired_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRequired.Text))
+            {
+                MessageBox.Show("Field Required harus diisi!");
+                return;
+            }
+        }
+
+        private void txtRegex_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Regex.IsMatch(txtRegex.Text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                MessageBox.Show("Format email tidak valid!");
+                return;
+            }
+        }
+
+        private void txtA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtA.Text != txtB.Text)
+            {
+                MessageBox.Show("TextBox A dan B harus sama!");
+                return;
+            }
+        }
+
+        private void txtB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtA.Text != txtB.Text)
+            {
+                MessageBox.Show("TextBox A dan B harus sama!");
+                return;
+            }
+        }
+
+        private void txtLength_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtLength.Text.Length < 5)
+            {
+                MessageBox.Show("Minimal 5 karakter!");
+                return;
+            }
+        }
+
+        private void txtUpper_TextChanged(object sender, EventArgs e)
+        {
+            int pos = txtUpper.SelectionStart;
+            txtUpper.Text = txtUpper.Text.ToUpper();
+            txtUpper.SelectionStart = pos;
+        }
+
+        private void txtLower_TextChanged(object sender, EventArgs e)
+        {
+            int pos = txtLower.SelectionStart;
+            txtLower.Text = txtLower.Text.ToLower();
+            txtLower.SelectionStart = pos;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // REQUIRED
+            if (string.IsNullOrWhiteSpace(txtRequired.Text))
+            {
+                MessageBox.Show("Field Required harus diisi!");
+                return;
+            }
+
+            // REGEX
+            if (!Regex.IsMatch(txtRegex.Text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                MessageBox.Show("Format email tidak valid!");
+                return;
+            }
+
+            // COMPARISON
+            if (txtA.Text != txtB.Text)
+            {
+                MessageBox.Show("TextBox A dan B harus sama!");
+                return;
+            }
+
+            // LENGTH
+            if (txtLength.Text.Length < 5)
+            {
+                MessageBox.Show("Minimal 5 karakter!");
+                return;
+            }
+
+            // KALAU SEMUA VALID → TAMPILKAN MESSAGEBOX
+            string output =
+                $"Numeric      : {txtNumeric.Text}\n" +
+                $"Char         : {txtChar.Text}\n" +
+                $"Required     : {txtRequired.Text}\n" +
+                $"Regex/Email  : {txtRegex.Text}\n" +
+                $"A / B        : {txtA.Text} / {txtB.Text}\n" +
+                $"Length       : {txtLength.Text}\n" +
+                $"Upper Case   : {txtUpper.Text}\n" +
+                $"Lower Case   : {txtLower.Text}";
+
+            MessageBox.Show(output, "Hasil Form");
+        }
+
+    }
+}
